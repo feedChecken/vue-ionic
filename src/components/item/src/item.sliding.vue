a576e6d2704df8deaa2f1706e1413162ee5fee2d
谢仁洪 <xierenyuan@qq.com>
2016-11-07 14:08:25 +0800

    item sliding 完善..

--- src/components/item/src/item.sliding.vue
+++ src/components/item/src/item.sliding.vue
@@ -1,10 +1,6 @@
 <template>
-    <div v-clickoutside:touchstart="onSwipeMove"
-         @click="onSwipeMove()" 
-         @touchstart="onStartDrag"
-         @touchmove="onDrag"
-         @touchend="endDrag"
-         class="ion-item-sliding item-wrapper" >
+    <div v-clickoutside:touchstart="onSwipeMove" @click="onSwipeMove()" @touchstart="onStartDrag" @touchmove="onDrag" @touchend="endDrag"
+        class="ion-item-sliding item-wrapper" :class="[dragging ? 'active-slide active-options-right' : '' ]">
         <slot></slot>
     </div>
 </template>
@@ -24,7 +20,8 @@
                 start: {
                     x: 0,
                     y: 0
-                }
+                },
+                dragging: false
             }
         },
         mounted() {
@@ -43,22 +40,80 @@
             this.itemWrap = this.itemComponent.$el;
             this.rightWrap = this.itemOptionsComponent.$el;
             this.rightWidth = this.itemOptionsComponent.width();
+            this.rightDefaultTransform = this.translate3d(0);
         },
         methods: {
             translate3d(offset) {
                 return `translate3d(${offset}px, 0, 0)`;
             },
-            onSwipeMove() {
+            onSwipeMove(offset = 0) {
                 console.log('onSwipeMove');
+                this.itemWrap.style.webkitTransform = this.translate3d(offset);
+                if (offset === 0) {
+                    this.dragging = false;
+                    this.start.x = 0;
+                    this.start.y = 0;
+                    this.offsetLeft = 0;
+                }
+                this.swiping = true;
             },
             onStartDrag(evt) {
-                console.log('onStartDrag', evt);
+                console.log('onStartDrag');
+                evt = evt.changedTouches ? evt.changedTouches[0] : evt;
+                this.dragging = true;
+                this.start.x = evt.pageX;
+                this.start.y = evt.pageY;
             },
             onDrag(evt) {
-                console.log('onDrag', evt);
+                if (this.opened) {
+                    !this.swiping && this.onSwipeMove(0);
+                    this.opened = false;
+                    return;
+                }
+                if (!this.dragging) return;
+                let swiping;
+                const e = evt.changedTouches ? evt.changedTouches[0] : evt;
+                const offsetTop = e.pageY - this.start.y;
+                const offsetLeft = this.offsetLeft = e.pageX - this.start.x;
+                const x = Math.abs(offsetLeft);
+                // 只做了 向从右到左 滑动  所以如果是从左到右滑 不处理
+                if (offsetLeft > 0 || x > this.rightWidth || !this.rightWidth) {
+                    return;
+                }
+                const y = Math.abs(offsetTop);
+                swiping = !(x < 5 || (x >= 5 && y >= x * 1.73));
+                if (!swiping) return;
+                evt.preventDefault();
+                this.onSwipeMove(offsetLeft);
             },
             endDrag(evt) {
-                console.log('endDrag', evt);
+                console.log('endDrag');
+                if (!this.swiping) return;
+                //如果 this.offsetLeft > 0 是从左向右滑动 小于0 是从右到左滑动
+                this.swipeLeaveTransition(this.offsetLeft > 0 ? 1 : -1);
+            },
+            swipeLeaveTransition(direction) {
+                setTimeout(() => {
+                    this.swipeLeave = true;
+                    const oLeft = Math.abs(this.offsetLeft);
+                    console.log(oLeft);
+                    //从右到左滑动 如果滑动的距离超过按钮组的百分之40 则 显示出按钮来
+                    if (direction < 0 && oLeft > this.rightWidth * 0.4) {
+                        this.onSwipeMove(-this.rightWidth);
+                        this.swiping = false;
+                        this.opened = true;
+                        return;
+                    }
+                    console.log('233 334');
+                    this.onSwipeMove(0);
+                    once(this.itemWrap, 'webkitTransitionEnd', _ => {
+                        console.log('233');
+                        this.itemWrap.style.webkitTransform = this.rightDefaultTransform;
+                        //this.rightWrap.style.webkitTransform = this.rightDefaultTransform;
+                        this.swipeLeave = false;
+                        this.swiping = false;
+                    });
+                }, 0);
             }
 
         }
